9a27dcd97cedb54878a865a33a3774c150a197c1
ts.naranmandakh <ts.naranmandakh@gmail.com>
2019-05-30 14:53:00 +0900

    #11 үйлчилгээний дэд таб уудтай файлыг нэгтгэсэн.

--- app/lib/src/widgets/tab_selector.dart
+++ app/lib/src/widgets/tab_selector.dart
@@ -1,30 +1,31 @@
-//import 'package:flutter/material.dart';
-//
-//class TabSelector extends StatelessWidget {
-//  final TabState activeTab;
-//  final Function(TabState) onTabSelected;
-//
-//  TabSelector({
-//    Key key,
-//    @required this.activeTab,
-//    @required this.onTabSelected,
-//  }) :  assert(onTabSelected != null),
-//        assert(activeTab != null),
-//        super(key: key);
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return BottomNavigationBar(
-//      currentIndex: TabState.values.indexOf(activeTab),
-//      onTap: (index) => onTabSelected(TabState.values[index]),
-//      items: TabState.values.map((tab) {
-//        return BottomNavigationBarItem(
-//          icon: Icon(
-//            tab == TabState.SERVICE ? Icons.rss_feed : (tab == TabState.NOTIFICATION ? Icons.access_alarm : Icons.menu),
-//          ),
-//          title: Text(""),
-//        );
-//      }).toList(),
-//    );
-//  }
-//}
+import 'package:ddish/src/models/tab_models.dart';
+import 'package:flutter/material.dart';
+
+class TabSelector extends StatelessWidget {
+  final TabState activeTab;
+  final Function(TabState) onTabSelected;
+
+  TabSelector({
+    Key key,
+    @required this.activeTab,
+    @required this.onTabSelected,
+  }) :  assert(onTabSelected != null),
+        assert(activeTab != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: TabState.values.indexOf(activeTab),
+      onTap: (index) => onTabSelected(TabState.values[index]),
+      items: TabState.values.map((tab) {
+        return BottomNavigationBarItem(
+          icon: Icon(
+            tab == TabState.SERVICE ? Icons.rss_feed : (tab == TabState.NOTIFICATION ? Icons.access_alarm : Icons.menu),
+          ),
+          title: Text(""),
+        );
+      }).toList(),
+    );
+  }
+}
