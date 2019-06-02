43fa93714a0a79b9e1820fbd87b4be6afd7dafd2
ts.naranmandakh <ts.naranmandakh@gmail.com>
2019-06-03 03:09:04 +0900

    #17 base_bloc class removed.

--- app/lib/src/blocs/service/service_bloc.dart
+++ app/lib/src/blocs/service/service_bloc.dart
@@ -3,8 +3,6 @@ import 'package:ddish/src/blocs/service/service_event.dart';
 import 'package:ddish/src/blocs/service/service_state.dart';
 import 'package:ddish/src/models/tab_models.dart';
 
-import '../base_bloc_state.dart';
-
 class ServiceBloc extends Bloc<ServiceEvent, ServiceState> {
   ///Үйлчилгээ цонхны default tab нь БАГЦ байна
   @override
