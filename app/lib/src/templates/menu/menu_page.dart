2f57a9183a9e06cb6ca79e99895f256768f1385c
z.bat-erdene <z.bat-erdene@chinggis.systems>
2019-06-03 19:12:40 +0800

    Menu #53

--- app/lib/src/templates/menu/menu_page.dart
+++ app/lib/src/templates/menu/menu_page.dart
@@ -10,6 +10,7 @@ class MenuPage extends StatefulWidget {
   final NavigationBloc navigationBloc;
 
   MenuPage({Key key, @required this.navigationBloc}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() => MenuPageState();
 }
@@ -17,33 +18,13 @@ class MenuPage extends StatefulWidget {
 class MenuPageState extends State<MenuPage> {
   MenuBloc menuBloc;
   NavigationBloc _navigationBloc;
-  List<Menu> menuItems = <Menu>[
-    Menu(
-      title: 'Антен тохируулах заавар', screen :null,
-      children: <Menu>[Menu(title:'Зурган заавар', screen: Order('Зурган заавар')), Menu(title: 'Видео заавар', screen: Order('Видео заавар'))],
-    ),
-    Menu(
-      title: 'Захиалга өгөх',
-      children: <Menu>[
-        Menu(title: 'Антен тохируулах захиалга өгөх'),
-        Menu(title: 'Шинэ хэрэглэгчийн захиалга өгөх')
-      ],
-    ),
-    Menu(
-      title: 'Мэдээ урамшуулах',
-    ),
-    Menu(
-      title: 'Салбарын мэдээлэл',
-    ),
-    Menu(
-      title: '7777-1434',
-    ),
-  ];
+  List<Menu> menuItems;
   Order orderWidget;
 
   @override
   void initState() {
     menuBloc = MenuBloc();
+    menuItems = initMenu();
     _navigationBloc = widget.navigationBloc;
     super.initState();
   }
@@ -56,36 +37,53 @@ class MenuPageState extends State<MenuPage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      bloc: menuBloc,
-      child: BlocBuilder<MenuEvent, MenuState>(
+    return Container(
+      padding: const EdgeInsets.only(top: 80.0),
+      child: BlocProvider(
         bloc: menuBloc,
-        builder: (BuildContext context, MenuState state) {
-          if(state is MenuOpened && state.menu.screen != null) {
-            return state.menu.screen;
-          }
-          if(state is MenuInitial) {
-            return ListView.builder(
-              itemCount: menuItems.length,
-              itemBuilder: (BuildContext context, int index) =>
-                  _buildMenuItem(menuItems[index]),
-            );
-          }
-        }),
+        child: BlocBuilder<MenuEvent, MenuState>(
+            bloc: menuBloc,
+            builder: (BuildContext context, MenuState state) {
+              if (state is MenuOpened && state.menu.screen != null) {
+                return state.menu.screen;
+              }
+              if (state is MenuInitial) {
+                return ListView.builder(
+                    itemCount: menuItems.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Column(
+                        children: <Widget>[
+                          _buildMenuItem(menuItems[index], true),
+                          Container(
+                            margin:
+                            EdgeInsetsDirectional.only(start: 15.0, end: 15.0),
+                            height: 1.0,
+                            color: Color(0xFF3069b2),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+              }
+            }),
+      ),
     );
   }
 
-  Widget _buildMenuItem(Menu menu) {
+  Widget _buildMenuItem(Menu menu, bool root) {
+    bool expanded = false;
     if (menu.children == null || menu.children.isEmpty)
       return ListTile(
+        contentPadding: root ? null : const EdgeInsets.only(left: 30.0),
         title: _buildTitle(menu.title),
         onTap: () => onMenuTap(menu),
       );
     return ExpansionTile(
+      onExpansionChanged: (v) => expanded = v,
       key: PageStorageKey<Menu>(menu),
       trailing: SizedBox.shrink(),
       title: _buildTitle(menu.title),
-      children: menu.children.map(_buildMenuItem).toList(),
+      children: menu.children.map((m) => _buildMenuItem(m, false)).toList(),
     );
   }
 
@@ -93,17 +91,50 @@ class MenuPageState extends State<MenuPage> {
     _navigationBloc.dispatch(NavigationEvent.INACTIVE);
     menuBloc.dispatch(MenuClicked(selectedMenu: menu));
   }
+
   _buildTitle(String title) {
-    return Text(
-      title,
-      style: TextStyle(
-//                color: const Color(0xffe4f0ff),
-          color: Colors.indigoAccent,
-          fontWeight: FontWeight.w400,
-          fontFamily: "Montserrat",
-          fontStyle: FontStyle.normal,
-          fontSize: 15.0),
+    return Container(
+      color:  null,
+      child: Text(
+        title,
+        style: TextStyle(
+            color: Color(0xFFe4f0ff),
+            fontWeight: FontWeight.w400,
+            fontFamily: "Montserrat",
+            fontStyle: FontStyle.normal,
+            fontSize: 15.0),
+      ),
     );
+  }
+
+  List initMenu() {
+    List<Menu> menuItems = <Menu>[
+      Menu(
+        title: 'Антен тохируулах заавар',
+        screen: null,
+        children: <Menu>[
+          Menu(title: 'Зурган заавар', screen: Order('Зурган заавар')),
+          Menu(title: 'Видео заавар', screen: Order('Видео заавар'))
+        ],
+      ),
+      Menu(
+        title: 'Захиалга өгөх',
+        children: <Menu>[
+          Menu(title: 'Антен тохируулах захиалга өгөх'),
+          Menu(title: 'Шинэ хэрэглэгчийн захиалга өгөх')
+        ],
+      ),
+      Menu(
+        title: 'Мэдээ урамшуулах',
+      ),
+      Menu(
+        title: 'Салбарын мэдээлэл',
+      ),
+      Menu(
+        title: '7777-1434',
+      ),
+    ];
+    return menuItems;
   }
 }
 
