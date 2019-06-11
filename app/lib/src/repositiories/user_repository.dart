60c24d32c16e266d0d9663644e5235e047941312
z.bat-erdene <z.bat-erdene@chinggis.systems>
2019-06-11 09:29:58 +0800

    Ddish oauth login #89

--- app/lib/src/repositiories/user_repository.dart
+++ app/lib/src/repositiories/user_repository.dart
@@ -1,12 +1,15 @@
+import 'dart:io';
+
 import 'package:ddish/src/api/user_api_provider.dart';
 import 'package:meta/meta.dart';
 import 'package:oauth2/oauth2.dart' as oauth2;
 import 'globals.dart' as globals;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
-class UserRepository{
+class UserRepository {
   final userApiProvider = UserApiProvider();
-  final authorizationEndpoint = Uri.parse(globals.serverEndpoint + "/oauth/token");
+  final authorizationEndpoint = Uri.parse(globals.serverEndpoint + "/login");
   oauth2.Client client;
 
   Future<String> authenticate({
@@ -15,23 +18,20 @@ class UserRepository{
     @required bool rememberUsername,
     @required bool useFingerprint,
   }) async {
-    final clientId = "ddish-oauth2-client";
-    final clientSecret = "ddish-oauth2-secret-password1234";
-
     var client;
+
     try {
       client = await oauth2.resourceOwnerPasswordGrant(
-          authorizationEndpoint, username, password,
-          identifier: clientId, secret: clientSecret);
-    } on oauth2.AuthorizationException catch(e){
+          authorizationEndpoint, username, password);
+    } on Exception catch (e) {
       throw e;
     }
 
     globals.client = client;
     var sharedPref = await SharedPreferences.getInstance();
-    if(rememberUsername)
+    if (rememberUsername)
       sharedPref.setString('username', username);
-    else if(sharedPref.getString('username') != null)
+    else if (sharedPref.getString('username') != null)
       sharedPref.remove('username');
 
     sharedPref.setBool('useFingerprint', useFingerprint);
