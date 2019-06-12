9acef0d205d816db761a56f7fe7226ff4f109405
ts.naranmandakh <ts.naranmandakh@gmail.com>
2019-06-13 03:56:00 +0900

    #88 Салбаруудын байршлыг map дээр харуулах V1

--- app/ios/Runner/AppDelegate.m
+++ app/ios/Runner/AppDelegate.m
@@ -1,13 +1,13 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyC9LWJiaH87CFipXkxxgUVfD49Lzf-dXM8"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
-
 @end
