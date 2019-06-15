9fcd7e4ab062f04acfb98e89ce70d2374fbfa633
BATERDENE-PC <batka0812@gmail.com>
2019-06-15 22:57:30 +0800

    Tab style #101

--- app/lib/src/templates/service/service_page.dart
+++ app/lib/src/templates/service/service_page.dart
@@ -1,9 +1,10 @@
-import 'package:ddish/src/blocs/service/pack/pack_bloc.dart';
+import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
 import 'package:ddish/src/blocs/service/service_bloc.dart';
 import 'package:ddish/src/blocs/service/service_event.dart';
 import 'package:ddish/src/blocs/service/service_state.dart';
 import 'package:ddish/src/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'account/account_page.dart';
@@ -28,16 +29,27 @@ class ServicePageState extends State<ServicePage> {
 
   ///Үйлчилгээний үндсэн таб ууд
   TabBar get createTabBar => TabBar(
+        labelPadding: EdgeInsets.symmetric(horizontal: 30.0),
+        indicatorSize: TabBarIndicatorSize.label,
         isScrollable: true,
         tabs: serviceTabs
-            .map((tabItem) => Tab(child: Text(tabItem.title)))
+            .map((tabItem) => Tab(
+                  text: tabItem.title,
+                ))
             .toList(),
         onTap: (tabIndex) =>
             bloc.dispatch(ServiceTabSelected(serviceTabs[tabIndex].state)),
-        labelStyle: TextStyle(
-            color: Color(0xfff9f9f9),
-            backgroundColor: Color.fromRGBO(42, 104, 184, 1)),
-        indicator: UnderlineTabIndicator(borderSide: BorderSide(width: 0)),
+        labelStyle: const TextStyle(
+            color:  const Color(0xfff9f9f9),
+            fontWeight: FontWeight.w400,
+            fontFamily: "Montserrat",
+            fontStyle:  FontStyle.normal,
+            fontSize: 14.0,
+        ),
+        indicator: BubbleTabIndicator(
+            indicatorColor: Color(0xff2a68b8),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0)),
         indicatorColor: Colors.transparent,
         unselectedLabelStyle: TextStyle(color: Color(0xfff9f9f9)),
         unselectedLabelColor: Color(0xfff9f9f9),
@@ -61,7 +73,6 @@ class ServicePageState extends State<ServicePage> {
 
   Widget createBuilder(BuildContext context, ServiceState state) {
     this.servicePackTabState = state;
-
     return Container(
       margin: EdgeInsets.fromLTRB(8, 10, 8, 4),
       child: DefaultTabController(
@@ -83,14 +94,16 @@ class ServicePageState extends State<ServicePage> {
             resizeToAvoidBottomPadding: true,
             backgroundColor: Colors.transparent,
             body: Padding(
-              padding: const EdgeInsets.only(left: 10.0, right: 10.0, bottom: 70.0),
+              padding:
+                  const EdgeInsets.only(left: 10.0, right: 10.0, bottom: 70.0),
               child: Container(
                 padding: const EdgeInsets.all(10.0),
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: new BorderRadius.circular(20.0),
                 ),
-                child: TabBarView(children: [AccountPage(), PackPage(), MoviePage()]),
+                child: TabBarView(
+                    children: [AccountPage(), PackPage(), MoviePage()]),
               ),
             ),
           ),
@@ -98,6 +111,7 @@ class ServicePageState extends State<ServicePage> {
       ),
     );
   }
+
   @override
   void dispose() {
     bloc.dispose();
