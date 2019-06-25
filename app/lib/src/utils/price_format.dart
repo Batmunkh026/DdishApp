cd342d7c158f5932a979d376f30c858176a0d10c
ts.naranmandakh <ts.naranmandakh@gmail.com>
2019-06-25 17:09:15 +0900

    #123 format error fixed.

--- app/lib/src/utils/price_format.dart
+++ app/lib/src/utils/price_format.dart
@@ -4,8 +4,20 @@ class PriceFormatter {
     if (price == null || (price is String && price.isEmpty)) return "0";
 
     var values = price.toString().split(".");
+    if (values.isEmpty) return price;
 
-    return values[0].replaceAllMapped(RegExp(r"^([\d]+)([\d]{3}'[\d]+|[\d]{3})$"),
-        (match) => "${match.group(1)}'${match.group(2)}")+"${values.length > 1 ? ".${values[1]}" : ""}";
+    return "${insertPrecisionChar(values[0])}${values.length > 1 ? ".${values[1]}" : ""}";
+  }
+
+  static String insertPrecisionChar(String value) {
+    if (value == null || value.isEmpty) return "";
+
+    var newValue = value.replaceAllMapped(
+        RegExp(r"^([\d]+)([\d]{3}'[\d]+|[\d]{3})$"),
+        (match) => "${match.group(1)}'${match.group(2)}");
+
+    if (!newValue.startsWith(RegExp(r"[\d]{4,}"))) return newValue;
+
+    return insertPrecisionChar(newValue);
   }
 }
