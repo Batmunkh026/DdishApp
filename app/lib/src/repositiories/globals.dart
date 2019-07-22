136a66c0d80afc8acf54c3d273e9bd2a8e20cda3
Batmunkh <batmunkh@ddishtv.mn>
2019-07-22 14:27:28 +0800

    serverEndpoint хаяг сольсон

--- app/lib/src/repositiories/globals.dart
+++ app/lib/src/repositiories/globals.dart
@@ -3,5 +3,6 @@ library app.globals;
 import 'package:oauth2/oauth2.dart' as oauth2;
 
 oauth2.Client client;
-var serverEndpoint = "http://my.ddishtv.mn:8008/api";
+//var serverEndpoint = "http://my.ddishtv.mn:8008/api";
+var serverEndpoint = "https://my.ddishtv.mn/api";
 String authorizationToken = 'YGHM9SHBC81LMR4G';
