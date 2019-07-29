5495cab5c094f28f6664d790251d041ee208b8ad
ts.naranmandakh <ts.naranmandakh@gmail.com>
2019-07-29 14:57:08 +0900

    #161 loading indicator нэмсэн

--- app/lib/src/templates/menu/promo/antennInstallationManual.dart
+++ app/lib/src/templates/menu/promo/antennInstallationManual.dart
@@ -7,78 +7,77 @@ import 'package:ddish/src/repositiories/promo_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-
-class AntennaWidget extends StatefulWidget{
-
+class AntennaWidget extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => AntennaWidgetState();
 }
 
-class AntennaWidgetState extends State<AntennaWidget>{
+class AntennaWidgetState extends State<AntennaWidget> {
   AntennaBloc _bloc;
   AntennRepository _repository;
   List<AntennMdl> manuals;
   //List<String> manuals;
 
   @override
-  void initState(){
+  void initState() {
     manuals = List<AntennMdl>();
     //manuals = List<String>();
     _repository = AntennRepository();
     _bloc = AntennaBloc(repository: _repository);
     super.initState();
   }
+
   @override
-  void dispose(){
+  void dispose() {
     _bloc.dispose();
     super.dispose();
   }
 
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     return BlocBuilder<AntennaEvent, AntennaState>(
       bloc: _bloc,
-      builder: (BuildContext context, AntennaState state){
-        if(state is AntennaWidgetLoading){
+      builder: (BuildContext context, AntennaState state) {
+        if (state is AntennaWidgetLoading) {
           return Expanded(
             child: Center(
               child: CircularProgressIndicator(),
             ),
           );
         }
-        if(state is AntennaWidgetStarted){
+        if (state is AntennaWidgetStarted) {
           _bloc.dispatch(AntennaEventStarted());
         }
-        if(state is AntennaWidgetLoaded){
+        if (state is AntennaWidgetLoaded) {
           manuals = state.manualList;
-          return manuals !=null ?
-          Flexible(
-            child: Container(
-              padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
-              child: ListView.builder(
-                itemCount: manuals.length,
-                itemBuilder: (BuildContext context, int index){
-                  return Padding(
-                    padding: const EdgeInsets.only(bottom: 20.0),
-                    child: GestureDetector(
-                      child: Container(
-                        padding: const EdgeInsets.all(300.0),//(150.0, 520.0, 0.0, 0.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          image: DecorationImage(
-                            fit: BoxFit.fill,
-                            //image:AssetImage(manuals[index].imageUrl),
-                            image: CachedNetworkImageProvider(manuals[index].imageUrl),
-                          )
-                        ),
-                      ),
+          return manuals != null
+              ? Flexible(
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+                    child: ListView.builder(
+                      itemCount: manuals.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(bottom: 20.0),
+                          child: GestureDetector(
+                            child: Container(
+                              child: CachedNetworkImage(
+                                imageUrl: manuals[index].imageUrl,
+                                placeholder: (context, url) =>
+                                    new LinearProgressIndicator(),
+                                errorWidget: (context, url, error) =>
+                                    new Icon(Icons.error),
+                              ),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20.0)),
+                            ),
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
-            ),
-          ) :Container();
+                  ),
+                )
+              : Container();
         }
         return Container();
       },
--- app/lib/src/templates/menu/promo/newPromoInfo.dart
+++ app/lib/src/templates/menu/promo/newPromoInfo.dart
@@ -8,14 +8,12 @@ import 'package:ddish/src/repositiories/promo_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-
-class PromoWidget extends StatefulWidget{
-
+class PromoWidget extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => PromoWidgetState();
 }
 
-class PromoWidgetState extends State<PromoWidget>{
+class PromoWidgetState extends State<PromoWidget> {
   PromoBloc _bloc;
   PromoRepository _repository;
   List<NewPromoMdl> promotions;
@@ -23,87 +21,90 @@ class PromoWidgetState extends State<PromoWidget>{
   bool loading = false;
 
   @override
-  void initState(){
+  void initState() {
     promotions = List<NewPromoMdl>();
     _repository = PromoRepository();
     _bloc = PromoBloc(repository: _repository);
     super.initState();
   }
+
   @override
-  void dispose(){
+  void dispose() {
     _bloc.dispose();
     super.dispose();
   }
 
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     //final height = MediaQuery.of(context).size.height;
     //final ImageProvider _imageProvider;
     return BlocBuilder<PromoEvent, PromoState>(
       bloc: _bloc,
-      builder: (BuildContext context, PromoState state){
-        if(state is PromoWidgetLoading){
+      builder: (BuildContext context, PromoState state) {
+        if (state is PromoWidgetLoading) {
           return Expanded(
             child: Center(
               child: CircularProgressIndicator(),
             ),
           );
         }
-        if(state is PromoWidgetStarted){
+        if (state is PromoWidgetStarted) {
           _bloc.dispatch(PromoStarted());
         }
-        if(state is PromoWidgetLoaded){
+        if (state is PromoWidgetLoaded) {
           promotions = state.promoList;
-          return promotions != null ?
-          Flexible(
-            child: Container(
-              padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
-              child: ListView.builder(
-                itemCount: promotions.length,
-                itemBuilder: (BuildContext context, int index){
-                  return Padding(
-                    padding: const EdgeInsets.only(bottom: 20.0),
-                    child: GestureDetector(
-                      child: Container(
-                        padding: EdgeInsets.all(80.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20.0),
-                          image: DecorationImage(
-                            fit: BoxFit.fill,
-                            image: CachedNetworkImageProvider(promotions[index].PromoPosterUrl),
-                          ),
-                        ),
-                      ),
-                      onTap: ()=> onPromotionTap(promotions[index]),
-                    ),
-                  );
-                },
-              )
-            ),
-          ):Container();
+          return promotions != null
+              ? Flexible(
+                  child: Container(
+                      padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+                      child: ListView.builder(
+                        itemCount: promotions.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(bottom: 20.0),
+                            child: GestureDetector(
+                              child: Container(
+                                child: CachedNetworkImage(
+                                  imageUrl: promotions[index].PromoPosterUrl,
+                                  placeholder: (context, url) =>
+                                      new LinearProgressIndicator(),
+                                  errorWidget: (context, url, error) =>
+                                      new Icon(Icons.error),
+                                ),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20.0)),
+                              ),
+                              onTap: () => onPromotionTap(promotions[index]),
+                            ),
+                          );
+                        },
+                      )),
+                )
+              : Container();
         }
-        if(state is PromoWidgetTapped){
+        if (state is PromoWidgetTapped) {
           return Flexible(
             child: Container(
               padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
               child: ListView.builder(
                 itemCount: selectedPromo.detials.length,
-                itemBuilder: (BuildContext context, int index){
+                itemBuilder: (BuildContext context, int index) {
                   return Padding(
                     padding: const EdgeInsets.only(bottom: 10.0),
                     child: GestureDetector(
                       child: Container(
-                        padding: const EdgeInsets.all(300.0),//(150.0, 480.0, 0.0, 0.0),
+                        padding: const EdgeInsets.all(
+                            300.0), //(150.0, 480.0, 0.0, 0.0),
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20.0),
                           image: DecorationImage(
                             fit: BoxFit.fill,
-                            image: CachedNetworkImageProvider(selectedPromo.detials[index].PromoDetialPosterUrl),
+                            image: CachedNetworkImageProvider(selectedPromo
+                                .detials[index].PromoDetialPosterUrl),
                           ),
                         ),
                       ),
-                      onTap: ()=> onPromotionDetialTap(selectedPromo),
+                      onTap: () => onPromotionDetialTap(selectedPromo),
                     ),
                   );
                 },
@@ -111,25 +112,24 @@ class PromoWidgetState extends State<PromoWidget>{
             ),
           );
         }
-        if(state is PromoWidgetDetialTapped){
+        if (state is PromoWidgetDetialTapped) {
           return Container(
-            height: (MediaQuery.of(context).size.height*0.75),
-            width: (MediaQuery.of(context).size.width*0.9),
+            height: (MediaQuery.of(context).size.height * 0.75),
+            width: (MediaQuery.of(context).size.width * 0.9),
             alignment: Alignment.topCenter,
             child: new ListView(
               reverse: false,
               children: <Widget>[
                 RichText(
                   text: TextSpan(
-                    text: selectedPromo.PromoDescText,
-                    style: const TextStyle(
+                      text: selectedPromo.PromoDescText,
+                      style: const TextStyle(
                         color: const Color(0xffFFFFF0),
                         fontWeight: FontWeight.bold,
                         fontStyle: FontStyle.normal,
                         //fontSize: 16.0,
                         fontFamily: 'Montserrat',
-                    )
-                  ),
+                      )),
                 )
               ].reversed.toList(),
             ),
@@ -138,12 +138,14 @@ class PromoWidgetState extends State<PromoWidget>{
         return Container();
       },
     );
- }
-  onPromotionTap(NewPromoMdl promo){
+  }
+
+  onPromotionTap(NewPromoMdl promo) {
     selectedPromo = promo;
     _bloc.dispatch(PromoTapped());
   }
-  onPromotionDetialTap(NewPromoMdl promo){
+
+  onPromotionDetialTap(NewPromoMdl promo) {
     selectedPromo = promo;
     _bloc.dispatch(PromoDetialTapped());
   }
