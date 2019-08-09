cfe558d59167eac8ec10fba8e6c42148700fefd4
ts.naranmandakh <ts.naranmandakh@gmail.com>
2019-08-09 14:15:57 +0900

    #201 өөр сонголт оруулах /number keyboard/

--- app/lib/src/templates/service/product/custom_option_page.dart
+++ app/lib/src/templates/service/product/custom_option_page.dart
@@ -33,17 +33,36 @@ class CustomProductChooserState extends State<CustomProductChooser>
   ProductBloc _bloc;
   String paymentPreview = '0';
   int month;
-  StreamController<int> monthStreamController = StreamController();
+  TextEditingController monthEditingController;
 
   CustomProductChooserState() {
-    monthStreamController.stream.listen((month) {
-      this.month = month;
-      if (_bloc.currentState.selectedProductTab ==
-          ProductTabType.UPGRADE) //server ээс авах
-        _bloc.dispatch(CustomMonthChanged(_bloc.currentState.selectedProductTab,
-            _bloc.selectedProduct, _bloc.currentState.selectedProduct, month));
-      else
-        paymentPreview = "${(month * widget.priceToExtend)}";
+    monthEditingController = TextEditingController();
+
+    monthEditingController.addListener(() {
+      var value = monthEditingController.text;
+      bool isEmptyOrNull = value == null || value.isEmpty;
+
+      if (value != '${month}') {
+        if (isEmptyOrNull || value == '0') {
+          setState(() {
+            paymentPreview = "";
+            month = 0;
+          });
+        } else {
+          month = Converter.toInt(value);
+          if (_bloc.currentState.selectedProductTab ==
+              ProductTabType.UPGRADE) //server ээс авах
+            _bloc.dispatch(CustomMonthChanged(
+                _bloc.currentState.selectedProductTab,
+                _bloc.selectedProduct,
+                _bloc.currentState.selectedProduct,
+                month));
+          else
+            setState(
+                () => paymentPreview = "${(month * widget.priceToExtend)}");
+        }
+      }
+      print("month value: " + value);
     });
   }
 
@@ -56,12 +75,18 @@ class CustomProductChooserState extends State<CustomProductChooser>
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
+    var state = _bloc.currentState;
+
     if (widget.isPaymentComputed) {
       paymentPreview = "${widget.priceToExtend}";
       month = widget.monthToExtend;
+
+      var monthStr = "${month == null ? '' : month}";
+      if (state.selectedProductTab == ProductTabType.UPGRADE)
+        monthEditingController.text = monthStr;
+      widget.isPaymentComputed = false;
     }
 
-    var state = _bloc.currentState;
     var isUpgradeOrChannel =
         state.selectedProductTab == ProductTabType.ADDITIONAL_CHANNEL ||
             state.selectedProductTab == ProductTabType.UPGRADE;
@@ -92,78 +117,79 @@ class CustomProductChooserState extends State<CustomProductChooser>
         : label;
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Center(
-        child: Form(
-          key: _formKey,
-          child: Align(
-            alignment: Alignment.topCenter,
-            child: ListView(
-              shrinkWrap: true,
-              children: <Widget>[
-                FlatButton(
-                  child: Stack(
-                    alignment: Alignment.centerRight,
-                    children: <Widget>[
-                      Align(
-                        child: Constants.appIcons[AppIcons.Back],
-                        alignment: Alignment.centerLeft,
-                      ),
-                      Align(
-                        child: Container(
-                          width: MediaQuery.of(context).size.width * 0.55,
-                          child: backComponent,
-                          padding: EdgeInsets.only(bottom: 10),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+        child: Center(
+          child: Form(
+            key: _formKey,
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  FlatButton(
+                    child: Stack(
+                      alignment: Alignment.centerRight,
+                      children: <Widget>[
+                        Align(
+                          child: Constants.appIcons[AppIcons.Back],
+                          alignment: Alignment.centerLeft,
                         ),
-                        alignment: Alignment.topCenter,
-                      ),
-                    ],
+                        Align(
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.55,
+                            child: backComponent,
+                            padding: EdgeInsets.only(bottom: 10),
+                          ),
+                          alignment: Alignment.topCenter,
+                        ),
+                      ],
+                    ),
+                    //TODO back to previous page
+                    onPressed: _bloc.backToPrevState,
                   ),
-                  //TODO back to previous page
-                  onPressed: _bloc.backToPrevState,
-                ),
-                Center(
-                    child: Column(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.3,
-                      height: MediaQuery.of(context).size.height * 0.055,
-                      padding: EdgeInsets.only(top: 5),
-                      child: InputField(
-                        hasBorder: true,
-                        align: TextAlign.center,
-                        initialValue: '${month == null ? '' : month}',
-                        textInputType: TextInputType.text,
-                        inputFormatters: [
-                          InputValidations
-                              .acceptedFormatters[InputType.NumberInt],
-                          WhitelistingTextInputFormatter(
-                              RegExp(r'(^1[0-2]$)|(^[0-9]$)'))
-                        ],
-                        onFieldSubmitted: (value) =>
-                            monthStreamController.add(Converter.toInt(value)),
+                  Center(
+                      child: Column(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.3,
+                        height: MediaQuery.of(context).size.height * 0.055,
+                        padding: EdgeInsets.only(top: 5),
+                        child: InputField(
+                          hasBorder: true,
+                          align: TextAlign.center,
+                          textInputType: TextInputType.number,
+                          inputFormatters: [
+                            InputValidations
+                                .acceptedFormatters[InputType.NumberInt],
+                            WhitelistingTextInputFormatter(
+                                RegExp(r'(^1[0-2]$)|(^[0-9]$)'))
+                          ],
+                          textController: monthEditingController,
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 15),
-                      child: Text(
-                        "Сунгах сарын үнийн дүн",
-                        style: TextStyle(fontSize: 10),
+                      Padding(
+                        padding: EdgeInsets.only(top: 15),
+                        child: Text(
+                          "Сунгах сарын үнийн дүн",
+                          style: TextStyle(fontSize: 10),
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 15, bottom: 15),
-                      child: Text(
-                          "₮${PriceFormatter.productPriceFormat(paymentPreview)}",
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                    ),
-                    SubmitButton(
-                        text: "Сунгах",
-                        onPressed: () => _toExtend(state),
-                        verticalMargin: 0,
-                        horizontalMargin: 0)
-                  ],
-                )),
-              ],
+                      Padding(
+                        padding: EdgeInsets.only(top: 15, bottom: 15),
+                        child: Text(
+                            "₮${PriceFormatter.productPriceFormat(paymentPreview)}",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                      SubmitButton(
+                          text: "Сунгах",
+                          onPressed: () => _toExtend(state),
+                          verticalMargin: 0,
+                          horizontalMargin: 0)
+                    ],
+                  )),
+                ],
+              ),
             ),
           ),
         ),
@@ -183,13 +209,13 @@ class CustomProductChooserState extends State<CustomProductChooser>
             ? _bloc.selectedProduct.name
             : state.selectedProduct.name;
 
-    openPermissionDialog(_bloc, context, event, productName,
-        month, widget.priceToExtend);
+    openPermissionDialog(
+        _bloc, context, event, productName, month, widget.priceToExtend);
   }
 
   @override
   void dispose() {
-    monthStreamController.close();
+    monthEditingController.dispose();
     super.dispose();
   }
 }
